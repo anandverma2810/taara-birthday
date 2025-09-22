a51a72dc542a63141aefd41b2deb75a0fb56d6ad
Anand Verma <anand.verma@3scsolutions.com>
2025-09-23 00:26:50 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // optional: use @ for src
+    },
+  },
+  build: {
+    assetsInlineLimit: 0, // ensures mp4 and other media are copied, not inlined as base64
+  },
 })
